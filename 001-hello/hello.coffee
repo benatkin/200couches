7e8399ec0ae632cd35468941c96afe998805087c
ben@benatkin.com <ben@benatkin.com>
2010-10-26 17:10:56 -0600

    implement dummy downloading

--- 001-hello/hello.coffee
+++ 001-hello/hello.coffee
@@ -1,12 +1,30 @@
-hello =
+Hello =
   layout:
     scripts:
-      "jquery.js":
-        src:     "https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"
-        hotlink: true
-      "underscore.js": "http://documentcloud.github.com/underscore/underscore-min.js"
-      "backbone.js":   "http://documentcloud.github.com/backbone/backbone-min.js"
-  ready: () ->
-    console.log "Hello from Node!"
+      "jquery.js":     "https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"
+      "underscore.js": true
+      "backbone.js":   true
+  files:
+    "underscore-min.js": "http://documentcloud.github.com/underscore/underscore-min.js"
+    "backbone-min.js":   "http://documentcloud.github.com/backbone/backbone-min.js"
+  Pusher:
+    download: (files, remaining) ->
+      if remaining.length == 0
+        return
+      
+      file = remaining[0]
+      console.log("Downloading #{file}")
+      this.download(files, remaining.slice(1))
 
-hello.ready()
+    push: () ->
+      files = this.root.files
+      remaining = []
+      for file, url of files
+        remaining.push(file)
+      this.download(this.root.files, remaining)
+
+  init: () ->
+    this.Pusher.root = this.Pusher.parent = this
+    this.Pusher.push()
+
+Hello.init()
--- 001-hello/hello.js
+++ 001-hello/hello.js
@@ -1,19 +1,45 @@
 (function() {
-  var hello;
-  hello = {
+  var Hello;
+  var __hasProp = Object.prototype.hasOwnProperty;
+  Hello = {
     layout: {
       scripts: {
-        "jquery.js": {
-          src: "https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",
-          hotlink: true
-        },
-        "underscore.js": "http://documentcloud.github.com/underscore/underscore-min.js",
-        "backbone.js": "http://documentcloud.github.com/backbone/backbone-min.js"
+        "jquery.js": "https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",
+        "underscore.js": true,
+        "backbone.js": true
       }
     },
-    ready: function() {
-      return console.log("Hello from Node!");
+    files: {
+      "underscore-min.js": "http://documentcloud.github.com/underscore/underscore-min.js",
+      "backbone-min.js": "http://documentcloud.github.com/backbone/backbone-min.js"
+    },
+    Pusher: {
+      download: function(files, remaining) {
+        var file;
+        if (remaining.length === 0) {
+          return null;
+        }
+        file = remaining[0];
+        console.log("Downloading " + (file));
+        return this.download(files, remaining.slice(1));
+      },
+      push: function() {
+        var _ref, file, files, remaining, url;
+        files = this.root.files;
+        remaining = [];
+        _ref = files;
+        for (file in _ref) {
+          if (!__hasProp.call(_ref, file)) continue;
+          url = _ref[file];
+          remaining.push(file);
+        }
+        return this.download(this.root.files, remaining);
+      }
+    },
+    init: function() {
+      this.Pusher.root = (this.Pusher.parent = this);
+      return this.Pusher.push();
     }
   };
-  hello.ready();
+  Hello.init();
 }).call(this);
