b8990f8fae3f41bdc4f7dc955c919eb3272fd219
ben@benatkin.com <ben@benatkin.com>
2010-10-26 18:09:09 -0600

    implemented downloading

--- 001-hello/hello.coffee
+++ 001-hello/hello.coffee
@@ -4,7 +4,7 @@ Hello =
       "jquery.js":     "https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"
       "underscore.js": true
       "backbone.js":   true
-  files:
+  attachments:
     "underscore-min.js": "http://documentcloud.github.com/underscore/underscore-min.js"
     "backbone-min.js":   "http://documentcloud.github.com/backbone/backbone-min.js"
   Starter:
@@ -20,16 +20,30 @@ Hello =
       url = this.url.parse(files[file])
       console.log("Downloading #{url.pathname} from #{url.hostname}")
 
-      this.download(files, remaining.slice(1))
+      chunks = []
+      self = this
+
+      server = this.http.createClient(url.port or 80, url.hostname)
+      request = server.request('GET', url.pathname, {'host': url.hostname})
+      request.end()
+      request.on 'response', (response) ->
+        response.setEncoding 'utf8'
+        response.on 'data', (chunk) ->
+          chunks.push(chunk)
+        response.on 'end', () ->
+          self.fs.writeFileSync file, chunks.join('')
+          self.download(files, remaining.slice(1))
 
     start: () ->
-      this.url = require('url')
+      this.url  = require('url')
+      this.http = require('http')
+      this.fs   = require('fs')
 
-      files = this.root.files
+      files = this.root.attachments
       remaining = []
       for file, url of files
         remaining.push(file)
-      this.download(this.root.files, remaining)
+      this.download(files, remaining)
 
   init: () ->
     this.Starter.root = this.Starter.parent = this
--- 001-hello/hello.js
+++ 001-hello/hello.js
@@ -9,26 +9,44 @@
         "backbone.js": true
       }
     },
-    files: {
+    attachments: {
       "underscore-min.js": "http://documentcloud.github.com/underscore/underscore-min.js",
       "backbone-min.js": "http://documentcloud.github.com/backbone/backbone-min.js"
     },
     Starter: {
       doc: "Starter downloads some needed files and initializes and starts Pusher.",
       download: function(files, remaining) {
-        var file, url;
+        var chunks, file, request, self, server, url;
         if (remaining.length === 0) {
           return null;
         }
         file = remaining[0];
         url = this.url.parse(files[file]);
         console.log("Downloading " + (url.pathname) + " from " + (url.hostname));
-        return this.download(files, remaining.slice(1));
+        chunks = [];
+        self = this;
+        server = this.http.createClient(url.port || 80, url.hostname);
+        request = server.request('GET', url.pathname, {
+          'host': url.hostname
+        });
+        request.end();
+        return request.on('response', function(response) {
+          response.setEncoding('utf8');
+          response.on('data', function(chunk) {
+            return chunks.push(chunk);
+          });
+          return response.on('end', function() {
+            self.fs.writeFileSync(file, chunks.join(''));
+            return self.download(files, remaining.slice(1));
+          });
+        });
       },
       start: function() {
         var _ref, file, files, remaining, url;
         this.url = require('url');
-        files = this.root.files;
+        this.http = require('http');
+        this.fs = require('fs');
+        files = this.root.attachments;
         remaining = [];
         _ref = files;
         for (file in _ref) {
@@ -36,7 +54,7 @@
           url = _ref[file];
           remaining.push(file);
         }
-        return this.download(this.root.files, remaining);
+        return this.download(files, remaining);
       }
     },
     init: function() {
